3b3188fbebbc3b847795907ce072c4fbd4e2c404
Nice3point <nice3point@gmail.com>
2026-01-24 00:15:21 +0300

    [Installer] Support Revit 2027 installation path

--- install/Installer.cs
+++ install/Installer.cs
@@ -8,19 +8,19 @@ const string projectName = "RevitLookup";
 
 var guidMap = new Dictionary<int, string>
 {
-    { 2015, "1C877362-19E8-4E10-A4B0-802BA88C1F3E" },
-    { 2016, "230933BA-3865-41C8-80E1-CFA0EB08B44B" },
-    { 2017, "A2402F72-90B0-4803-B783-06487D6BFBEB" },
-    { 2018, "2997B545-391C-41B2-A90A-E5C6BB39087A" },
-    { 2019, "DBF3C66B-B624-4E0B-B86D-44857B19CD0C" },
-    { 2020, "36D21BA1-C945-4D40-83B9-4C2518FC40EA" },
-    { 2021, "9B7FD05D-C782-4538-A5D3-04B64AE81FE4" },
-    { 2022, "207733B1-1BEA-4603-99EA-EA1E87077F60" },
-    { 2023, "2179ECCB-0ED3-4FFF-907D-01C9D57AD20D" },
-    { 2024, "2E347D52-D08D-4624-8909-3679D75B9C1D" },
-    { 2025, "5182B068-1BE3-42A9-B91D-BF4BEEE81680" },
-    { 2026, "468ECD03-68D6-4D99-B04C-13D72AB47CBC" },
-    { 2027, "8CCB4872-7F50-43C9-B3FE-50FEF55F96F4" },
+    {2015, "1C877362-19E8-4E10-A4B0-802BA88C1F3E"},
+    {2016, "230933BA-3865-41C8-80E1-CFA0EB08B44B"},
+    {2017, "A2402F72-90B0-4803-B783-06487D6BFBEB"},
+    {2018, "2997B545-391C-41B2-A90A-E5C6BB39087A"},
+    {2019, "DBF3C66B-B624-4E0B-B86D-44857B19CD0C"},
+    {2020, "36D21BA1-C945-4D40-83B9-4C2518FC40EA"},
+    {2021, "9B7FD05D-C782-4538-A5D3-04B64AE81FE4"},
+    {2022, "207733B1-1BEA-4603-99EA-EA1E87077F60"},
+    {2023, "2179ECCB-0ED3-4FFF-907D-01C9D57AD20D"},
+    {2024, "2E347D52-D08D-4624-8909-3679D75B9C1D"},
+    {2025, "5182B068-1BE3-42A9-B91D-BF4BEEE81680"},
+    {2026, "468ECD03-68D6-4D99-B04C-13D72AB47CBC"},
+    {2027, "8CCB4872-7F50-43C9-B3FE-50FEF55F96F4"},
 };
 
 var versioning = Versioning.CreateFromVersionStringAsync(args[0]);
@@ -60,7 +60,7 @@ void BuildSingleUserMsi()
     project.OutFileName = $"{outputName}-{versioning.Version}-SingleUser";
     project.Dirs =
     [
-        new InstallDir($@"%AppDataFolder%\Autodesk\Revit\Addins\{versioning.VersionPrefix.Major}", wixEntities)
+        new InstallDir($@"%AppDataFolder%\Autodesk\Revit\Addins", wixEntities)
     ];
     project.BuildMsi();
 }
@@ -71,7 +71,7 @@ void BuildMultiUserUserMsi()
     project.OutFileName = $"{outputName}-{versioning.Version}-MultiUser";
     project.Dirs =
     [
-        new InstallDir($@"%ProgramFiles%\Autodesk\Revit\Addins\{versioning.VersionPrefix.Major}", wixEntities)
+        new InstallDir(versioning.VersionPrefix.Major >= 2027 ? @"%ProgramFiles%\Autodesk\Revit\Addins" : @"%CommonAppDataFolder%\Autodesk\Revit\Addins", wixEntities)
     ];
     project.BuildMsi();
 }
