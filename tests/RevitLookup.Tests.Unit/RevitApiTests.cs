b16d6f67c1ca1d376a78ccc00db27c6a284f0827
Nice3point <nice3point@gmail.com>
2026-02-03 17:30:03 +0300

    [Tests] Remove project reference to core module.

--- tests/RevitLookup.Tests.Unit/RevitApiTests.cs
+++ tests/RevitLookup.Tests.Unit/RevitApiTests.cs
@@ -12,92 +12,92 @@
 // THERE IS NO GUARANTEE THAT THE OPERATION OF THE PROGRAM WILL BE
 // UNINTERRUPTED OR ERROR FREE.
 
-using Nice3point.TUnit.Revit;
-using Nice3point.TUnit.Revit.Executors;
-using RevitLookup.Core;
-using TUnit.Core.Executors;
-
-namespace RevitLookup.Tests.Unit;
-
-public sealed class RevitApiTests : RevitApiTest
-{
-    private static string SamplesPath => $@"C:\Program Files\Autodesk\Revit {Application.VersionNumber}\Samples";
-
-    [Before(Class)]
-    public static void ValidateSamples()
-    {
-        if (!Directory.Exists(SamplesPath))
-        {
-            Skip.Test($"Samples folder not found at {SamplesPath}");
-            return;
-        }
-
-        if (!Directory.EnumerateFiles(SamplesPath, "*.rfa").Any())
-        {
-            Skip.Test($"No .rfa files found in {SamplesPath}");
-        }
-    }
-
-    public static IEnumerable<string> GetSampleFile()
-    {
-        if (!Directory.Exists(SamplesPath))
-        {
-            yield return string.Empty;
-            yield break;
-        }
-
-        yield return Directory.EnumerateFiles(SamplesPath, "*.rfa")
-            .OrderByDescending(file => new FileInfo(file).Length)
-            .First();
-    }
-
-    [Test]
-    [TestExecutor<RevitThreadExecutor>]
-    [MethodDataSource(nameof(GetSampleFile))]
-    public async Task Parameters_Builtin_ShouldCreateAllCategories(string filePath)
-    {
-        Document? document = null;
-
-        try
-        {
-            document = Application.OpenDocumentFile(filePath);
-
-            var builtInParameters = Enum.GetValues<BuiltInParameter>();
-            foreach (var builtInParameter in builtInParameters)
-            {
-                var parameter = RevitShell.GetBuiltinParameter(document, builtInParameter);
-
-                await Assert.That(parameter).IsNotNull();
-            }
-        }
-        finally
-        {
-            document?.Close(false);
-        }
-    }
-
-    [Test]
-    [TestExecutor<RevitThreadExecutor>]
-    [MethodDataSource(nameof(GetSampleFile))]
-    public async Task Categories_Builtin_ShouldCreateAllCategories(string filePath)
-    {
-        Document? document = null;
-
-        try
-        {
-            document = Application.OpenDocumentFile(filePath);
-
-            var builtInCategories = Enum.GetValues<BuiltInCategory>();
-            foreach (var builtInCategory in builtInCategories)
-            {
-                var category = RevitShell.GetBuiltinCategory(document, builtInCategory);
-
-                await Assert.That(category).IsNotNull();
-            }
-        }
-        finally
-        {
-            document?.Close(false);
-        }
-    }
-}
+// using Nice3point.TUnit.Revit;
+// using Nice3point.TUnit.Revit.Executors;
+// using RevitLookup.Core;
+// using TUnit.Core.Executors;
+//
+// namespace RevitLookup.Tests.Unit;
+//
+// public sealed class RevitApiTests : RevitApiTest
+// {
+//     private static string SamplesPath => $@"C:\Program Files\Autodesk\Revit {Application.VersionNumber}\Samples";
+//
+//     [Before(Class)]
+//     public static void ValidateSamples()
+//     {
+//         if (!Directory.Exists(SamplesPath))
+//         {
+//             Skip.Test($"Samples folder not found at {SamplesPath}");
+//             return;
+//         }
+//
+//         if (!Directory.EnumerateFiles(SamplesPath, "*.rfa").Any())
+//         {
+//             Skip.Test($"No .rfa files found in {SamplesPath}");
+//         }
+//     }
+//
+//     public static IEnumerable<string> GetSampleFile()
+//     {
+//         if (!Directory.Exists(SamplesPath))
+//         {
+//             yield return string.Empty;
+//             yield break;
+//         }
+//
+//         yield return Directory.EnumerateFiles(SamplesPath, "*.rfa")
+//             .OrderByDescending(file => new FileInfo(file).Length)
+//             .First();
+//     }
+//
+//     [Test]
+//     [TestExecutor<RevitThreadExecutor>]
+//     [MethodDataSource(nameof(GetSampleFile))]
+//     public async Task Parameters_Builtin_ShouldCreateAllCategories(string filePath)
+//     {
+//         Document? document = null;
+//
+//         try
+//         {
+//             document = Application.OpenDocumentFile(filePath);
+//
+//             var builtInParameters = Enum.GetValues<BuiltInParameter>();
+//             foreach (var builtInParameter in builtInParameters)
+//             {
+//                 var parameter = RevitShell.GetBuiltinParameter(document, builtInParameter);
+//
+//                 await Assert.That(parameter).IsNotNull();
+//             }
+//         }
+//         finally
+//         {
+//             document?.Close(false);
+//         }
+//     }
+//
+//     [Test]
+//     [TestExecutor<RevitThreadExecutor>]
+//     [MethodDataSource(nameof(GetSampleFile))]
+//     public async Task Categories_Builtin_ShouldCreateAllCategories(string filePath)
+//     {
+//         Document? document = null;
+//
+//         try
+//         {
+//             document = Application.OpenDocumentFile(filePath);
+//
+//             var builtInCategories = Enum.GetValues<BuiltInCategory>();
+//             foreach (var builtInCategory in builtInCategories)
+//             {
+//                 var category = RevitShell.GetBuiltinCategory(document, builtInCategory);
+//
+//                 await Assert.That(category).IsNotNull();
+//             }
+//         }
+//         finally
+//         {
+//             document?.Close(false);
+//         }
+//     }
+// }
